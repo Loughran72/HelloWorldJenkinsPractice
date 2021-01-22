066c3559a9751efe20b4e1fd012c6d5b66e6236b
Irene Loughran <BD.Loughran@yahoo.com>
2021-01-22 12:34:16 -0600

    Initial Commit

---
+++ HelloWorldJan2021.java
@@ -0,0 +1,1 @@
+public class HelloWorldJan2021 {    public static void main(String[] args) {        //Prints "Hello World!!" in the terminal window        System.out.println("Hello World!!");    }}
